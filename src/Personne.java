aab72517e0b821586cf03d6c7e8a1e2f9479fa5d
Mouhamoud <mouhamoud.dembele@hotmail.com>
2026-04-04 09:16:59 +0000

    creating Personne class

---
+++ src/Personne.java
@@ -0,0 +1,75 @@
+public class Personne {
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String phone;
+    private int age;
+    private char sexe;
+
+    public Personne(String matricule, String nom, String prenom, String adresse, String phone, int age, char sexe) {
+        this.matricule = matricule;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.phone = phone;
+        this.age = age;
+        this.sexe = sexe;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(char sexe) {
+        this.sexe = sexe;
+    }
+}
